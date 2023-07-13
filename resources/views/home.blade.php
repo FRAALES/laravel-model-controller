ecee63dffdbca09f2c85ebafc767768f4599b19a
FRAALES <alessandrofranco1992@gmail.com>
2023-07-13 12:17:43 +0200

    ricontrollato tutto, sempre stesso errore

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.main-layout')
 
-@section('content')
+@section("content")
 
-<div class="container">
-    <h1>
-        Best Movies Ever
-    </h1>
-    <div class="card">
-        <div class="column">
-            <ul>
-                @foreach ($movies as $movie)
-                    <li>
-                        {{$movie -> id}}
-                        {{$movie -> title}}
-                        {{$movie -> original_title}}
-                        {{$movie -> nationality}}
-                        {{$movie -> date}}
-                        {{$movie -> vote}}
-                    </li>
-                @endforeach
-            </ul>
+    <div class="container">
+        <h1>
+            Best Movies Ever
+        </h1>
+        <div class="card">
+            <div class="column">
+                <ul>
+                    @foreach ($movies as $movie)
+                        <li>
+                            {{$movie -> id}}
+                            {{$movie -> title}}
+                            {{$movie -> original_title}}
+                            {{$movie -> nationality}}
+                            {{$movie -> date}}
+                            {{$movie -> vote}}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 
 @endsection

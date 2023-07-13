24ad0ac64fc7585d28f7ee1957c922bc1436d414
FRAALES <alessandrofranco1992@gmail.com>
2023-07-13 10:11:03 +0200

    mi arrendo

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -10,6 +10,8 @@
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     @vite('resources/js/app.js')
+    
+    @yield('header');
 </head>
 <body>
     @include('partials.header')
